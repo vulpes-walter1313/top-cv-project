c5657fa5acc7ef08c0acf21cc0987842d7f23c06
Walter Fritsche <wfritsche95@protonmail.com>
2021-10-07 14:28:17 -0500

    basic layout of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import EducationForm from './components/EducationForm';
+import ExperienceForm from './components/ExperienceForm';
+import GeneralInfo from './components/GeneralInfo';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: '',
+       email: '',
+       phone: '',
+       schoolName: '',
+       study: '', 
+       schoolStartYear: '',
+       schoolEndYear: '',
+       jobs: []
+    }
+    this.submitGenInfo = this.submitGenInfo.bind(this);
+    this.submitEduInfo = this.submitEduInfo.bind(this);
+    this.submitWorkInfo = this.submitWorkInfo.bind(this);
+  }
+
+  submitGenInfo(values) {
+    const {name, email, phone } = values;
+    this.setState({name: name, email: email, phone: phone});
+  }
+
+  submitEduInfo(values) {
+    const {
+      school: schoolName,
+      study,
+      startYear: schoolStartYear,
+      endYear: schoolEndYear } = values;
+
+      this.setState({ schoolName: schoolName,
+        study: study,
+        schoolStartYear: schoolStartYear,
+        schoolEndYear: schoolEndYear }, ()=> {
+          console.log(this.state);
+        });
+  }
+  submitWorkInfo(values) {
+    this.setState({jobs: [...values.jobs]}, () => console.log(this.state));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <GeneralInfo subFunc={this.submitGenInfo}/>
+        <EducationForm submitFunc={this.submitEduInfo}/>
+        <ExperienceForm submitFunc={this.submitWorkInfo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
