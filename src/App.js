6f83b2955ab4b54af214051d48984ca8985282d0
Walter Fritsche <wfritsche95@protonmail.com>
2021-10-08 18:04:17 -0500

    cleaned up state structure, updated edu info submit func, added edu info remove func

--- src/App.js
+++ src/App.js
@@ -13,15 +13,13 @@ class App extends React.Component {
        name: '',
        email: '',
        phone: '',
-       schoolName: '',
-       study: '', 
-       schoolStartYear: '',
-       schoolEndYear: '',
+       schools: [],
        jobs: []
     }
     this.submitGenInfo = this.submitGenInfo.bind(this);
     this.submitEduInfo = this.submitEduInfo.bind(this);
     this.submitWorkInfo = this.submitWorkInfo.bind(this);
+    this.removeEducation = this.removeEducation.bind(this);
   }
 
   submitGenInfo(values) {
@@ -29,22 +27,29 @@ class App extends React.Component {
     this.setState({name: name, email: email, phone: phone});
   }
 
-  submitEduInfo(values) {
-    const {
-      school: schoolName,
-      study,
-      startYear: schoolStartYear,
-      endYear: schoolEndYear } = values;
-
-      this.setState({ schoolName: schoolName,
-        study: study,
-        schoolStartYear: schoolStartYear,
-        schoolEndYear: schoolEndYear }, ()=> {
-          console.log(this.state);
-        });
+  submitEduInfo(value) {
+    this.setState({ schools: [...this.state.schools, 
+    {
+      school: value.school,
+      study: value.study,
+      startYear: value.startYear,
+      endYear: value.endYear,
+      id: Date.now()
+    }] }, ()=> {
+        console.log("Education info added:")
+        console.log(this.state);
+      });
   }
   submitWorkInfo(values) {
     this.setState({jobs: [...values.jobs]}, () => console.log(this.state));
+  }
+  removeEducation(idToRemove) {
+    this.setState(prevState => {
+      const newSchoolsArr = prevState.schools.filter(school => school.id != idToRemove)
+      return {
+        schools: newSchoolsArr
+      };
+    })
   }
 
   render() {
@@ -52,7 +57,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <GeneralInfo subFunc={this.submitGenInfo}/>
-        <EducationForm submitFunc={this.submitEduInfo}/>
+        <EducationForm submitFunc={this.submitEduInfo} schools={this.state.schools} removeFunc={this.removeEducation}/>
         <ExperienceForm submitFunc={this.submitWorkInfo}/>
       </div>
     );
