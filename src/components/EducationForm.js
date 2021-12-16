e0dbadad895e723d4e62b7164d99d0cbd11cbe88
Walter Fritsche <wfritsche95@protonmail.com>
2021-12-16 14:48:28 -0600

    converted from Class Component to Functional Component

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -1,117 +1,101 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import EduPinBlock from './EduPinBlock';
 
-class EducationForm extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-       school: '',
-       study: '', 
-       startYear: '',
-       endYear: '',
-    };
-    this.schoolOnChangeHandler = this.schoolOnChangeHandler.bind(this);
-    this.studyOnChangeHandler = this.studyOnChangeHandler.bind(this);
-    this.startYearOnChangeHandler = this.startYearOnChangeHandler.bind(this);
-    this.endYearOnChangeHandler = this.endYearOnChangeHandler.bind(this);
-    this.editSchoolInfo = this.editSchoolInfo.bind(this);
-    this.submitFormHandler = this.submitFormHandler.bind(this);
-  }
-  schoolOnChangeHandler(e) {
-    this.setState({school: e.target.value});
-  }
-  studyOnChangeHandler(e) {
-    this.setState({study: e.target.value});
-  }
-  startYearOnChangeHandler(e) {
-    this.setState({startYear: e.target.value});
-  }
-  endYearOnChangeHandler(e) {
-    this.setState({endYear: e.target.value});
-  }
-  editSchoolInfo(school, study, startYear, endYear) {
-    this.setState({
-      school,
-      study,
-      startYear,
-      endYear
-    });
-  }
-  submitFormHandler(e) {
-    e.preventDefault();
-    this.props.submitFunc(this.state);
-    this.setState({
-        school: '',
-        study: '',
-        startYear: '',
-        endYear: ''
-      });
-  }
-  
-  render() {
-    const education = this.props.schools.map(school => {
-      return (
-        <li key={school.id}>
-          <EduPinBlock schoolInfo={school}
-            removeFunc={this.props.removeFunc}
-            editFunc={this.editSchoolInfo}/>
-        </li>
-      );
-    });
+function EducationForm(props) {
+  const [school, setSchool] = useState('');
+  const [study, setStudy] = useState('');
+  const [startYear, setStartYear] = useState('');
+  const [endYear, setEndYear] = useState('');
 
-    return (
-      <div className="section-component">
-        <h2 className="section-title">Education Information</h2>
-        <hr/>
-        <form className="education-info-form" onSubmit={this.submitFormHandler}>
-          <div className="school-input-group">
-            <label htmlFor="school">School Name</label>
-            <input type="text"
-              id="school"
-              name="school"
-              value={this.state.school}
-              onChange={this.schoolOnChangeHandler}/>
-          </div>
-          <div className="study-input-group">
-            <label htmlFor="study">Area of Study</label>
-            <input type="text"
-              id="study"
-              name="study"
-              value={this.state.study}
-              onChange={this.studyOnChangeHandler}/>
-          </div>
-          <div className="dateStudy-input-group">
-            <label htmlFor="studystart">Start Year</label>
-            <input type="text"
-              id="studystart"
-              name="studystart"
-              value={this.state.startYear}
-              onChange={this.startYearOnChangeHandler}/>
-            <label htmlFor="studyend">End Year</label>
-            <input type="text"
-              id="studyend"
-              name="studyend"
-              value={this.state.endYear}
-              onChange={this.endYearOnChangeHandler}/>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-        {(()=> {
-          if (this.props.schools.length > 0) {
-            return (
-              <div className="submitted-pin-blocks">
-                <h3>Educational Info:</h3>
-                <ul>
-                  {education}
-                </ul>
-              </div>
-            );
-          }
-        })()}
-      </div>
-    );
+  function schoolOnChangeHandler(e) {
+    setSchool(e.target.value);
   }
+  function studyOnChangeHandler(e) {
+    setStudy(e.target.value);
+  }
+  function startYearOnChangeHandler(e) {
+    setStartYear(e.target.value);
+  }
+  function endYearOnChangeHandler(e) {
+    setEndYear(e.target.value);
+  }
+  function submitFormHandler(e) {
+    e.preventDefault();
+    props.submitFunc({ school, study, startYear, endYear });
+    setSchool('');
+    setStudy('');
+    setStartYear('');
+    setEndYear('');
+  }
+
+  function editSchoolInfo(school, study, startYear, endYear) {
+    setSchool(school);
+    setStudy(study);
+    setStartYear(startYear);
+    setEndYear(endYear);
+  }
+
+  const education = props.schools.map(school => {
+    return (
+      <li key={school.id}>
+        <EduPinBlock schoolInfo={school}
+          removeFunc={props.removeFunc}
+          editFunc={editSchoolInfo} />
+      </li>
+    );
+  });
+
+  return (
+    <div className="section-component">
+      <h2 className="section-title">Education Information</h2>
+      <hr />
+      <form className="education-info-form" onSubmit={submitFormHandler}>
+        <div className="school-input-group">
+          <label htmlFor="school">School Name</label>
+          <input type="text"
+            id="school"
+            name="school"
+            value={school}
+            onChange={schoolOnChangeHandler} />
+        </div>
+        <div className="study-input-group">
+          <label htmlFor="study">Area of Study</label>
+          <input type="text"
+            id="study"
+            name="study"
+            value={study}
+            onChange={studyOnChangeHandler} />
+        </div>
+        <div className="dateStudy-input-group">
+          <label htmlFor="studystart">Start Year</label>
+          <input type="text"
+            id="studystart"
+            name="studystart"
+            value={startYear}
+            onChange={startYearOnChangeHandler} />
+          <label htmlFor="studyend">End Year</label>
+          <input type="text"
+            id="studyend"
+            name="studyend"
+            value={endYear}
+            onChange={endYearOnChangeHandler} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {(() => {
+        if (props.schools.length > 0) {
+          return (
+            <div className="submitted-pin-blocks">
+              <h3>Educational Info:</h3>
+              <ul>
+                {education}
+              </ul>
+            </div>
+          );
+        }
+      })()}
+    </div>
+  );
 }
 
 export default EducationForm;
