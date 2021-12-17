f51954bebe82f552ff516bef39075946bbd0d79b
Walter Fritsche <wfritsche95@protonmail.com>
2021-12-16 18:12:38 -0600

    converted Class Component into Functional Component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,111 +1,98 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import StoreLocal from '../logic/StoreLocal';
 
-class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-       name: '',
-       email: '',
-       phone: '',
-       gender: 'Other',
-       dob: ''
-    };
-    this.changeNameHandler = this.changeNameHandler.bind(this);
-    this.changeEmailHandler = this.changeEmailHandler.bind(this);
-    this.changePhoneHandler = this.changePhoneHandler.bind(this);
-    this.changeGenderHandler = this.changeGenderHandler.bind(this);
-    this.changeDobHandler = this.changeDobHandler.bind(this);
-    this.formSubmit = this.formSubmit.bind(this)
+function GeneralInfo(props) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [gender, setGender] = useState('Other');
+  const [dob, setDob] = useState('');
+
+  function changeNameHandler(e) {
+    setName(e.target.value);
   }
-  componentDidMount() {
+  function changeEmailHandler(e) {
+    setEmail(e.target.value);
+  }
+  function changePhoneHandler(e) {
+    setPhone(e.target.value);
+  }
+  function changeGenderHandler(e) {
+    setGender(e.target.value);
+  }
+  function changeDobHandler(e) {
+    setDob(e.target.value);
+  }
+  function formSubmit(e) {
+    e.preventDefault();
+    props.subFunc({ name, email, phone, gender, dob });
+  }
+
+  // initial mount for data
+  useEffect(() => {
     const data = StoreLocal.loadState();
     if (data !== null) {
-      this.setState({
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        dob: data.dob,
-        gender: data.gender
-      });
+      setName(data.name);
+      setEmail(data.email);
+      setPhone(data.phone);
+      setDob(data.dob);
+      setGender(data.gender);
     }
-  }
-  changeNameHandler(e) {
-    this.setState({ name: e.target.value});
-  }
-  changeEmailHandler(e) {
-    this.setState({ email: e.target.value});
-  }
-  changePhoneHandler(e) {
-    this.setState({ phone: e.target.value});
-  }
-  changeGenderHandler(e) {
-    this.setState({gender: e.target.value});
-  }
-  changeDobHandler(e) {
-    this.setState({dob: e.target.value});
-  }
-  formSubmit(e) {
-    e.preventDefault();
-    this.props.subFunc(this.state);
-  }
-  
-  render() {
-    return (
-      <div className="section-component">
-        <h2 className="section-title">Basic Information</h2>
-        <hr/>
-        <form className="general-info-form" onSubmit={this.formSubmit}>
-          <div className="name-input-group">
-            <label htmlFor="name">Name</label>
-            <input type="text"
-              onChange={this.changeNameHandler}
-              id="name"
-              name="name"
-              value={this.state.name}/>
-          </div>
-          <div className="email-input-group">
-            <label htmlFor="email">Email</label>
-            <input type="text"
-              onChange={this.changeEmailHandler}
-              id="email"
-              name="email"
-              value={this.state.email}/>
-          </div>
-          <div className="phone-input-group">
-            <label htmlFor="phone">Phone</label>
-            <input type="text"
-              onChange={this.changePhoneHandler}
-              id="phone"
-              name="phone"
-              value={this.state.phone}/>
-          </div>
-          <div className="gender-input-group">
-            <label htmlFor="gender">Gender</label>
-            <select
-              onChange={this.changeGenderHandler}
-              id="gender"
-              name="gender"
-              value={this.state.gender}>
-                <option value="Other">Prefer Not To Say</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-            </select>
-          </div>
-          <div className="dob-input-group">
-            <label htmlFor="dob">Date of Birth</label>
-            <input type="text"
-              onChange={this.changeDobHandler}
-              id="dob"
-              name="dob"
-              value={this.state.dob}/>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+  }, []);
+
+  return (
+    <div className="section-component">
+      <h2 className="section-title">Basic Information</h2>
+      <hr />
+      <form className="general-info-form" onSubmit={formSubmit}>
+        <div className="name-input-group">
+          <label htmlFor="name">Name</label>
+          <input type="text"
+            onChange={changeNameHandler}
+            id="name"
+            name="name"
+            value={name} />
+        </div>
+        <div className="email-input-group">
+          <label htmlFor="email">Email</label>
+          <input type="text"
+            onChange={changeEmailHandler}
+            id="email"
+            name="email"
+            value={email} />
+        </div>
+        <div className="phone-input-group">
+          <label htmlFor="phone">Phone</label>
+          <input type="text"
+            onChange={changePhoneHandler}
+            id="phone"
+            name="phone"
+            value={phone} />
+        </div>
+        <div className="gender-input-group">
+          <label htmlFor="gender">Gender</label>
+          <select
+            onChange={changeGenderHandler}
+            id="gender"
+            name="gender"
+            value={gender}>
+            <option value="Other">Prefer Not To Say</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </div>
+        <div className="dob-input-group">
+          <label htmlFor="dob">Date of Birth</label>
+          <input type="text"
+            onChange={changeDobHandler}
+            id="dob"
+            name="dob"
+            value={dob} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default GeneralInfo
+export default GeneralInfo;
