6bd9642881d583ae24ee1ece4eca84517cace759
Walter Fritsche <wfritsche95@protonmail.com>
2021-12-20 16:37:06 -0600

    removed unreachable break statements in switch case and unnecessary comments

--- src/App.js
+++ src/App.js
@@ -7,113 +7,6 @@ import Header from './components/Header';
 import OutputDisplay from './components/OutputDisplay';
 import StoreLocal from './logic/StoreLocal';
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props)
-
-//     this.state = {
-//        name: '',
-//        email: '',
-//        phone: '',
-//        dob: '',
-//        gender: 'Other',
-//        schools: [],
-//        jobs: []
-//     }
-//     this.submitGenInfo = this.submitGenInfo.bind(this);
-//     this.submitEduInfo = this.submitEduInfo.bind(this);
-//     this.submitWorkInfo = this.submitWorkInfo.bind(this);
-//     this.removeEducation = this.removeEducation.bind(this);
-//     this.removeJob = this.removeJob.bind(this);
-//   }
-
-//   submitGenInfo(values) {
-//     const {name, email, phone, gender, dob } = values;
-//     this.setState({name: name, email: email, phone: phone, gender: gender, dob: dob});
-//   }
-//   componentDidUpdate() {
-//     StoreLocal.saveState(this.state);
-//   }
-//   componentDidMount() {
-//     const data = StoreLocal.loadState();
-//     console.log(data);
-//     if (data !== null) {
-//       this.setState({
-//         name: data.name,
-//         phone: data.phone,
-//         email: data.email,
-//         gender: data.gender,
-//         dob: data.dob,
-//         jobs: [...data.jobs],
-//         schools: [...data.schools]
-//       });
-//     }
-//   }
-//   submitEduInfo(value) {
-//     this.setState({ schools: [...this.state.schools, 
-//     {
-//       school: value.school,
-//       study: value.study,
-//       startYear: value.startYear,
-//       endYear: value.endYear,
-//       id: Date.now()
-//     }] }, ()=> {
-//         console.log("Education info added:")
-//         console.log(this.state);
-//       });
-//   }
-//   submitWorkInfo(values) {
-//     this.setState(prevState => {
-//       return {
-//         jobs: [...prevState.jobs, 
-//         {
-//           id: Date.now(),
-//           business: values.business,
-//           position: values.position,
-//           description: values.description,
-//           startDate: values.startDate,
-//           endDate: values.endDate
-//         }]
-//       }
-//     });
-//   }
-//   removeEducation(idToRemove) {
-//     this.setState(prevState => {
-//       const newSchoolsArr = prevState.schools.filter(school => school.id != idToRemove)
-//       return {
-//         schools: newSchoolsArr
-//       };
-//     });
-//   }
-//   removeJob(idToRemove) {
-//     this.setState(prevState => {
-//       const newJobsArr = prevState.jobs.filter(job => job.id != idToRemove)
-//       return {
-//         jobs: newJobsArr
-//       };
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <Header />
-//         <GeneralInfo subFunc={this.submitGenInfo}/>
-//         <EducationForm submitFunc={this.submitEduInfo} schools={this.state.schools} removeFunc={this.removeEducation}/>
-//         <ExperienceForm submitFunc={this.submitWorkInfo} jobs={this.state.jobs} removeFunc={this.removeJob}/>
-//         <OutputDisplay
-//           personName={this.state.name}
-//           email={this.state.email}
-//           phone={this.state.phone}
-//           gender={this.state.gender}
-//           dob={this.state.dob}
-//           schools={this.state.schools}
-//           jobs={this.state.jobs}
-//           />
-//       </div>
-//     );
-//   }
-// }
 const initialState = {
   name: '',
   email: '',
@@ -137,7 +30,6 @@ function reducer(state, action) {
         schools: [...data.schools],
         jobs: [...data.jobs]
       };
-      break;
     case 'GENINFO_SUBMIT':
       return {
         ...state,
@@ -147,7 +39,6 @@ function reducer(state, action) {
         gender: action.payload.gender,
         dob: action.payload.dob
       };
-      break;
     case 'EDUINFO_SUBMIT':
       return {
         ...state,
@@ -156,7 +47,6 @@ function reducer(state, action) {
           action.payload
         ]
       };
-      break;
     case 'EXPINFO_SUBMIT':
       return {
         ...state,
@@ -165,7 +55,6 @@ function reducer(state, action) {
           action.payload
         ]
       };
-      break;
     case 'REMOVE_EDU':
       const idToRemove = action.payload.id;
       const newSchools = state.schools.filter(school => school.id !== idToRemove);
@@ -173,7 +62,6 @@ function reducer(state, action) {
         ...state,
         schools: newSchools
       };
-      break;
     case 'REMOVE_WORK':
       const jobIdToRemove = action.payload.id;
       const newJobs = state.jobs.filter(job => job.id !== jobIdToRemove);
@@ -181,7 +69,6 @@ function reducer(state, action) {
         ...state,
         jobs: newJobs
       };
-      break;
     default:
       return state;
   }
